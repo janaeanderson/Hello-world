0bdbd49283d9c7124a2198c22c7b48f42493a786
Janae Anderson <janae.anderson.2022@student.lhschools.org>
2020-03-04 13:32:48 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,13 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is ");
+        System.out.print("Janae ");
+        System.out.println("Last summer I went swimming a lot, ");
+        System.out.println("learned to drive, ");
+        System.out.println("and I visited family.");
+    }
+}
